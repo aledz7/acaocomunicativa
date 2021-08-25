e90b8357b02f9785737b0ae3c8e1169d9fea5939
Kleber Souza <klebersouza@iMac-de-Kleber.local>
2021-08-25 17:11:21 -0300

    Favion added

--- resources/views/layouts/website.blade.php
+++ resources/views/layouts/website.blade.php
@@ -7,6 +7,9 @@
         <meta name="description" content="@yield('description') - Ação Comunicativa">
         <title> @yield('title') {{ config('app.name', 'Laravel') }}</title>
 
+        <link rel="icon" href="favicon.ico" />
+
+
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
         <link rel="stylesheet" href="/css/sharetastic.css">
