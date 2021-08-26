2267df9ece3104967f4e82dd3da27c599870087b
Kleber Souza <klebersouza@iMac-de-Kleber.local>
2021-08-26 10:50:59 -0300

    Text sidebar aligned

--- resources/views/side-bar.blade.php
+++ resources/views/side-bar.blade.php
@@ -1,13 +1,14 @@
 <div>
 	<div class="py-6">
-	<div class="flex-1 text-center bg-gray-100 py-4 px-2">
-		<div class="text-xl font-bold mb-2 text-center">
-			Newsletter
+		<div class="flex-1 text-center bg-gray-100 py-4 px-2">
+			<div class="text-xl font-bold mb-2 text-center">
+				Newsletter
+			</div>
+			<div class="mb-6 text-sm">
+				Cadastre-se para receber notícias no seu e-mail toda semana.
+			</div>
+			<a href='{{route("newsletter")}}'><button class="bg-actionblue px-2 py-1 rounded uppercase font-bold text-white">Cadastrar-se</button></a>
 		</div>
-		<div class="mb-6 text-sm">
-			Cadastre-se para receber notícias no seu e-mail toda semana.
-		</div>
-		<a href='{{route("newsletter")}}'><button class="bg-actionblue px-2 py-1 rounded uppercase font-bold text-white">Cadastrar-se</button></a>
 	</div>
 </div>
 
