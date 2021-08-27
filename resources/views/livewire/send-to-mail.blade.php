f5141b91f5017bccb462b3b97e71f4845a7fdcc0
Kleber Souza <klebersouza@iMac-de-Kleber.local>
2021-08-27 08:16:05 -0300

    Social Share icons size reduced

--- resources/views/livewire/send-to-mail.blade.php
+++ resources/views/livewire/send-to-mail.blade.php
@@ -1,6 +1,6 @@
 <div class="mx-1">
     <a @click='sendMail = true' class="text-gray-700  hover:text-blue-700 cursor-pointer transition duration-300 ease-in-out">
-        <svg xmlns="http://www.w3.org/2000/svg" class='w-6 h-6' fill='currentColor'   viewBox="0 0 1792 1792"><path d="M1792 710v794q0 66-47 113t-113 47h-1472q-66 0-113-47t-47-113v-794q44 49 101 87 362 246 497 345 57 42 92.5 65.5t94.5 48 110 24.5h2q51 0 110-24.5t94.5-48 92.5-65.5q170-123 498-345 57-39 100-87zm0-294q0 79-49 151t-122 123q-376 261-468 325-10 7-42.5 30.5t-54 38-52 32.5-57.5 27-50 9h-2q-23 0-50-9t-57.5-27-52-32.5-54-38-42.5-30.5q-91-64-262-182.5t-205-142.5q-62-42-117-115.5t-55-136.5q0-78 41.5-130t118.5-52h1472q65 0 112.5 47t47.5 113z"/></svg>
+        <svg xmlns="http://www.w3.org/2000/svg" class='w-5 h-5' fill='currentColor'   viewBox="0 0 1792 1792"><path d="M1792 710v794q0 66-47 113t-113 47h-1472q-66 0-113-47t-47-113v-794q44 49 101 87 362 246 497 345 57 42 92.5 65.5t94.5 48 110 24.5h2q51 0 110-24.5t94.5-48 92.5-65.5q170-123 498-345 57-39 100-87zm0-294q0 79-49 151t-122 123q-376 261-468 325-10 7-42.5 30.5t-54 38-52 32.5-57.5 27-50 9h-2q-23 0-50-9t-57.5-27-52-32.5-54-38-42.5-30.5q-91-64-262-182.5t-205-142.5q-62-42-117-115.5t-55-136.5q0-78 41.5-130t118.5-52h1472q65 0 112.5 47t47.5 113z"/></svg>
     </a>
     <div x-show='sendMail' class="fixed z-20 bg-black bg-opacity-50 left-0 right-0 top-0 bottom-0 py-10 px-4 sm:p-20" style="display: none;">
         <div x-show.transition='sendMail' @='sendMail = false'  class="bg-white rounded shadow-2xl sm:w-10/12 md:w-8/12 lg:w-3/12 mx-auto" >
